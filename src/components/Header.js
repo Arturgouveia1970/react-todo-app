31ed51489ca1d1523c362524824bb8da74fe683c
Arturgouveia1970 <artgouveiag@gmail.com>
2022-06-15 09:41:34 +0100

    changed to function based

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,17 @@
 import React from "react"
 
 const Header = () => {
+
+  const headerStyle = {
+    padding: "20px 0",
+    lineHeight: "1.5em",
+  }
+  
   return (
     <header style={headerStyle}>
-      <h1
-        style={{
-          fontSize: "6rem",
-          fontWeight: "600",
-          marginBottom: "2rem",
-          lineHeight: "1em",
-          color: "#ececec",
-          textTransform: "lowercase",
-          textAlign: "center",
-        }} 
-      >
-        todos
-      </h1>
+      <h1 style={{ fontSize: "6rem", fontWeight: "600", marginBottom: "2rem", lineHeight: "1em", color: "#eee", textTransform: "lowercase", textAlign: "center" }}>todos</h1>
     </header>
   )
 }
 
-const headerStyle = {
-  paddind: "20px 0",
-  lineHeight: "1.5em",
-}
-export default Header;
+export default Header
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,67 +1,67 @@
-import React from "react";
-import styles from "./TodoItem.module.css";
+import React, { useState, useEffect } from "react"
+import styles from "./TodoItem.module.css"
 
-class TodoItem extends React.Component {
-  handleEditing = () => {
-    this.setState({
-      editing: true,
-    })
+const TodoItem = props => {
+  const [editing, setEditing] = useState(false)
+
+  const handleEditing = () => {
+    setEditing(true)
   }
 
-  state = {
-    editing: false,
-  }
-  
-  handleUpdateDone = event => {
+  const handleUpdatedDone = event => {
     if (event.key === "Enter") {
-      this.setState({ editing: false })
+      setEditing(false)
     }
   }
 
-  componentWillUnmount() {
-    console.log("Cleaning up...")
+  useEffect(() => {
+    return () => {
+      console.log("Cleaning up...")
+    }
+  }, [])
+
+  const completedStyle = {
+    fontStyle: "italic",
+    color: "#595959",
+    opacity: 0.4,
+    textDecoration: "line-through",
   }
 
-  render() {
-    let viewMode = {};
-    let editMode = {};
-    if (this.state.editing) {
-      viewMode.display = "none";
-    } else {
-      editMode.display = "none";
-    }
-    const completedStyle = {
-      fontStyle: "italic",
-      color: "#595959",
-      opacity: 0.4,
-      textDecoration: "line-through",
-    }
-    const { completed, id, title } = this.props.todo;
-    return (
-      <li className={styles.item}>
+  const { completed, id, title } = props.todo
+
+  let viewMode = {}
+  let editMode = {}
+
+  if (editing) {
+    viewMode.display = "none"
+  } else {
+    editMode.display = "none"
+  }
+
+  return (
+    <li className={styles.item}>
+      <div onDoubleClick={handleEditing} style={viewMode}>
         <input
-          type="checkbox" 
+          type="checkbox"
           className={styles.checkbox}
           checked={completed}
-          onChange={() => this.props.handleChangeProps(id)}
+          onChange={() => props.handleChangeProps(id)}
         />
-        <button onClick={() => this.props.deleteTodoProps(id)}>Delete</button> 
-        <span style={this.props.todo.completed ? completedStyle : null}>
-          {title}
-        </span>
-        <div onDoubleClick={this.handleEditing} style={viewMode}>...</div>
-        <input
-          type="text"
-          style={editMode}
-          className={styles.textInput}
-          value={title}
-          onChange={e => {
-            this.props.setUpdate(e.target.value, id)
-          }}
-          onKeyDown={this.handleUpdateDone}
-        />
-      </li>
-    )
-  }
+        <button onClick={() => props.deleteTodoProps(id)}>Delete</button>
+        <span style={completed ? completedStyle : null}>{title}</span>
+      </div>
+      <input
+        type="text"
+        style={editMode}
+        className={styles.textInput}
+        value={title}
+        onChange={e => {
+          props.setUpdate(e.target.value, id)
+        }}
+        onKeyDown={handleUpdatedDone}
+      />
+    </li>
+  )
 }
-export default TodoItem;
+
+export default TodoItem
